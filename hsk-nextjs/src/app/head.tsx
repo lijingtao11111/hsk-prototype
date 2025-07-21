9bcbdb6d38daf07ddcee58c5ded3d4e47da40204
lijingtao <lijingtao@learnkg.com>
2025-07-21 10:08:32 +0800

    删除不再使用的环境配置文件、.gitignore、数据库模型、API路由及相关样式文件，清理项目结构以提高可维护性和简化代码库。

--- hsk-nextjs/src/app/head.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Head() {
-  return (
-    <>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" />
-    </>
-  );
-} 
